c6fdae486cd8bb5845e8d3011f2c0f0cf06a3cb8
Jamboree <tongari95@gmail.com>
2015-03-04 22:44:22 +0800

    Allow try-catch

--- include/co2/coroutine.hpp
+++ include/co2/coroutine.hpp
@@ -7,8 +7,10 @@
 #ifndef CO2_COROUTINE_HPP_INCLUDED
 #define CO2_COROUTINE_HPP_INCLUDED
 
+#include <atomic>
 #include <utility>
 #include <exception>
+#include <boost/assert.hpp>
 #include <boost/smart_ptr/intrusive_ptr.hpp>
 #include <boost/smart_ptr/intrusive_ref_counter.hpp>
 #include <boost/preprocessor/control/if.hpp>
@@ -40,11 +42,88 @@ namespace co2
 
 namespace co2 { namespace detail
 {
-    struct resumable_base : boost::intrusive_ref_counter<resumable_base>
+    namespace temp
     {
-        unsigned _next = 0;
+        static constexpr std::size_t bytes = sizeof(void*) * 4;
+        using storage = std::aligned_storage_t<bytes, alignof(std::max_align_t)>;
+
+        template<class T, bool fit>
+        struct traits_non_ref
+        {
+            static void create(void* p, T&& t)
+            {
+                new(p) T(std::move(t));
+            }
+
+            static T& get(void* p)
+            {
+                return *static_cast<T*>(p);
+            }
+
+            static void reset(void* p)
+            {
+                static_cast<T*>(p)->~T();
+            }
+        };
+
+        template<class T>
+        struct traits_non_ref<T, false>
+        {
+            static void create(void* p, T&& t)
+            {
+                *static_cast<T**>(p) = new T(std::move(t));
+            }
+
+            static T& get(void* p)
+            {
+                return **static_cast<T**>(p);
+            }
+
+            static void reset(void* p)
+            {
+                delete *static_cast<T**>(p);
+            }
+        };
+
+        template<class T>
+        struct traits : traits_non_ref<T, !(sizeof(T) > bytes) > {};
+
+        template<class T>
+        struct traits<T&>
+        {
+            static void create(void* p, T& t)
+            {
+                *static_cast<T**>(p) = &t;
+            }
+
+            static T& get(void* p)
+            {
+                return **static_cast<T**>(p);
+            }
+
+            static void reset(void* p) {}
+        };
+
+        template<class T>
+        struct auto_reset
+        {
+            void* tmp;
+
+            ~auto_reset()
+            {
+                traits<T>::reset(tmp);
+            }
+        };
+    }
+
+    struct resumable_base
+    {
+        temp::storage _tmp;
+        std::atomic<unsigned> _use_count {1};
+        unsigned _next;
+        unsigned _eh;
         virtual void run(coroutine<> const&) noexcept = 0;
-        virtual ~resumable_base() {}
+        virtual void release(coroutine<> const&) noexcept = 0;
     };
 
     template<class Promise>
@@ -67,23 +146,42 @@ namespace co2 { namespace detail
         F f;
 
         template<class Pack>
-        frame(Pack&& pack) : f(std::forward<Pack>(pack)) {}
+        frame(Pack&& pack) : f(std::forward<Pack>(pack))
+        {
+            _next = F::_co2_start::value;
+            _eh = F::_co2_stop::value;
+        }
 
         void run(coroutine<> const& coro) noexcept override
         {
-            try
-            {
-                f(static_cast<coroutine<Promise> const&>(coro), _next);
-            }
-            catch (...)
-            {
-                promise().set_exception(std::current_exception());
-            }
+            f(static_cast<coroutine<Promise> const&>(coro), _next, _eh, &_tmp);
+        }
+
+        void release(coroutine<> const& coro) noexcept override
+        {
+            f(static_cast<coroutine<Promise> const&>(coro), ++_next, _eh, &_tmp);
+            delete this;
         }
     };
     
     template<class R>
     using promise_t = typename R::promise_type;
+
+    struct avoid_plain_return
+    {
+        explicit avoid_plain_return() = default;
+    };
+
+    template<class Promise>
+    inline auto final_result(Promise* p) -> decltype(p->set_result())
+    {
+        p->set_result();
+    }
+
+    inline void final_result(void*)
+    {
+        BOOST_ASSERT_MSG(false, "missing return statement");
+    }
 }}
 
 namespace co2
@@ -93,11 +191,42 @@ namespace co2
     {
         struct promise_type;
 
-        explicit coroutine(detail::resumable_base* p) : _ptr(p) {}
+        coroutine() noexcept : _ptr() {}
+
+        coroutine(std::nullptr_t) noexcept : _ptr() {}
+
+        explicit coroutine(detail::resumable_base* ptr) noexcept : _ptr(ptr) {}
+
+        coroutine(coroutine const& other) : _ptr(other._ptr)
+        {
+            _ptr->_use_count.fetch_add(1, std::memory_order_relaxed);
+        }
+        
+        coroutine(coroutine&& other) noexcept : _ptr(other._ptr)
+        {
+            other._ptr = nullptr;
+        }
+
+        coroutine& operator=(coroutine other) noexcept
+        {
+            std::swap(_ptr, other._ptr);
+            return *this;
+        }
+
+        ~coroutine()
+        {
+            if (_ptr && _ptr->_use_count.fetch_sub(1, std::memory_order_relaxed) == 1)
+                _ptr->release(*this);
+        }
+
+        void swap(coroutine& other) noexcept
+        {
+            std::swap(_ptr, other._ptr);
+        }
 
         explicit operator bool() const noexcept
         {
-            return static_cast<bool>(_ptr);
+            return !!_ptr;
         }
 
         void operator()() const noexcept
@@ -107,7 +236,7 @@ namespace co2
 
     protected:
 
-        boost::intrusive_ptr<detail::resumable_base> _ptr;
+        detail::resumable_base* _ptr;
     };
 
     template<class Promise>
@@ -115,15 +244,24 @@ namespace co2
     {
         using promise_type = Promise;
 
-        explicit coroutine(detail::resumable<Promise>* p) : coroutine<>(p) {}
+        coroutine() = default;
 
-        explicit coroutine(Promise* p)
-            : coroutine<>(detail::resumable<Promise>::from_promise(p))
-        {}
+        coroutine(std::nullptr_t) noexcept {}
+
+        explicit coroutine(detail::resumable<Promise>* p) noexcept : coroutine<>(p) {}
+
+        explicit coroutine(Promise* p) noexcept
+        {
+            if (p)
+            {
+                _ptr = detail::resumable<Promise>::from_promise(p);
+                _ptr->_use_count.fetch_add(1, std::memory_order_relaxed);
+            }
+        }
 
         Promise& promise() const noexcept
         {
-            return static_cast<detail::resumable<Promise>*>(_ptr.get())->promise();
+            return static_cast<detail::resumable<Promise>*>(_ptr)->promise();
         }
     };
 
@@ -158,32 +296,40 @@ namespace co2
     };
 }
 
-#define CO2_AWAIT(ret, var)                                                     \
-if (!this->var.await_ready())                                                   \
+#define CO2_AWAIT_IMPL(ret, var, next)                                          \
 {                                                                               \
-    _co2_next = __LINE__;                                                       \
-    this->var.await_suspend(_co2_coro);                                         \
-    return;                                                                     \
+    using _co2_await = co2::detail::temp::traits<decltype(var)>;                \
+    _co2_await::create(_co2_tmp, var);                                          \
+    if (!_co2_await::get(_co2_tmp).await_ready())                               \
+    {                                                                           \
+        _co2_next = next;                                                       \
+        _co2_await::get(_co2_tmp).await_suspend(_co2_coro);                     \
+        return co2::detail::avoid_plain_return{};                               \
+    }                                                                           \
+    case next:                                                                  \
+    if (_co2_promise.cancellation_requested())                                  \
+    {                                                                           \
+        case __COUNTER__:                                                       \
+        _co2_await::reset(_co2_tmp);                                            \
+        return co2::detail::avoid_plain_return{};                               \
+    }                                                                           \
+    co2::detail::temp::auto_reset<decltype(var)> _co2_reset = {_co2_tmp};       \
+    ret (_co2_await::get(_co2_tmp).await_resume());                             \
 }                                                                               \
-case __LINE__:                                                                  \
-if (_co2_promise.cancellation_requested())                                      \
-    return;                                                                     \
-ret = this->var.await_resume();                                                 \
 /***/
 
-#define CO2_AWAIT_VOID(expr)                                                    \
+#define CO2_AWAIT(ret, var) CO2_AWAIT_IMPL(ret =, var, __COUNTER__)
+#define CO2_AWAIT_VOID(expr) CO2_AWAIT_IMPL(, expr, __COUNTER__)
+#define CO2_AWAIT_APPLY(let, var, ...)                                          \
+CO2_AWAIT_IMPL(([this](let) __VA_ARGS__), var, __COUNTER__)
+/***/
+
+#define CO2_RETURN(...)                                                         \
 {                                                                               \
-    auto&& var = expr;                                                          \
-    if (!var.await_ready())                                                     \
-    {                                                                           \
-        _co2_next = __LINE__;                                                   \
-        var.await_suspend(_co2_coro);                                           \
-        return;                                                                 \
-    }                                                                           \
+    _co2_next = _co2_stop::value;                                               \
+    _co2_promise.set_result(__VA_ARGS__);                                       \
+    return co2::detail::avoid_plain_return{};                                   \
 }                                                                               \
-case __LINE__:                                                                  \
-if (_co2_promise.cancellation_requested())                                      \
-    return;                                                                     \
 /***/
 
 #define CO2_LIST_MEMBER(r, _, e) e;
@@ -213,20 +359,61 @@ BOOST_PP_SEQ_FOR_EACH(macro, ~, BOOST_PP_VARIADIC_TO_SEQ t)
     _co2_pack pack = {CO2_TUPLE_FOR_EACH(CO2_FWD_MEMBER, capture)};             \
     struct _co2_op : _co2_pack                                                  \
     {                                                                           \
-        CO2_TUPLE_FOR_EACH(CO2_LIST_MEMBER, (__VA_ARGS__))                      \
+        __VA_ARGS__                                                             \
         _co2_op(_co2_pack&& pack) : _co2_pack(std::move(pack)) {}               \
-        void operator()(_co2_C const& _co2_coro, unsigned& _co2_next)           \
+        using _co2_start = std::integral_constant<unsigned, __COUNTER__>;       \
+        co2::detail::avoid_plain_return operator()                              \
+        (_co2_C const& _co2_coro, unsigned& _co2_next, unsigned& _co2_eh, void* _co2_tmp)\
         {                                                                       \
             auto& _co2_promise = _co2_coro.promise();                           \
-            switch (_co2_next)                                                  \
+            std::exception_ptr _co2_ex;                                         \
+            _co2_try_again:                                                     \
+            try                                                                 \
             {                                                                   \
-            case 0:                                                             \
-                CO2_AWAIT_VOID(_co2_promise.initial_suspend());                 \
+                switch (_co2_next)                                              \
+                {                                                               \
+                case _co2_start::value:                                         \
+                    CO2_AWAIT_VOID(_co2_promise.initial_suspend());             \
+                    using _co2_curr_eh = _co2_stop;                             \
+/***/
+
+#define CO2_TRY                                                                 \
+__pragma(warning(push))                                                         \
+__pragma(warning(disable:4456))                                                 \
+using _co2_prev_eh = _co2_curr_eh;                                              \
+using _co2_curr_eh = std::integral_constant<unsigned, __COUNTER__>;             \
+__pragma(warning(pop))                                                          \
+_co2_eh = _co2_curr_eh::value;                                                  \
+if (true)                                                                       \
+/***/
+
+#define CO2_CATCH                                                               \
+else case _co2_curr_eh::value:                                                  \
+    try                                                                         \
+    {                                                                           \
+        _co2_eh = _co2_prev_eh::value;                                          \
+        std::rethrow_exception(_co2_ex);                                        \
+    }                                                                           \
+    catch                                                                       \
 /***/
 
 #define CO2_END                                                                 \
+                    _co2_next = _co2_stop::value;                               \
+                    co2::detail::final_result(&_co2_promise);                   \
+                    break;                                                      \
+                case _co2_stop::value:                                          \
+                    _co2_promise.set_exception(_co2_ex);                        \
+                }                                                               \
             }                                                                   \
+            catch (...)                                                         \
+            {                                                                   \
+                _co2_next = _co2_eh;                                            \
+                _co2_ex = std::current_exception();                             \
+                goto _co2_try_again;                                            \
+            }                                                                   \
+            return co2::detail::avoid_plain_return{};                           \
         }                                                                       \
+        using _co2_stop = std::integral_constant<unsigned, __COUNTER__>;        \
     };                                                                          \
     _co2_C coro(new co2::detail::frame<_co2_P, _co2_op>(std::move(pack)));      \
     coro();                                                                     \
