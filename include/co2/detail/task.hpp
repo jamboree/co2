1824cf5a5463c0e1f2f40dd869a25cab3a222779
jamboree <tongari95@gmail.com>
2015-06-30 10:14:35 +0800

    Revert "Add task_error"
    This reverts commit cc21b5ae5b9c13b2071accb5730d219fe03f0621.

--- include/co2/detail/task.hpp
+++ include/co2/detail/task.hpp
@@ -9,7 +9,6 @@
 
 #include <atomic>
 #include <co2/coroutine.hpp>
-#include <co2/task_error.hpp>
 #include <co2/detail/storage.hpp>
 
 namespace co2
@@ -143,10 +142,8 @@ namespace co2 { namespace task_detail
             _coro.reset();
         }
 
-        bool await_ready() const
+        bool await_ready() const noexcept
         {
-            if (!_coro)
-                throw task_error(std::error_code(task_errc::no_state));
             return _coro.promise()._tag.load(std::memory_order_relaxed) != tag::null;
         }
 
