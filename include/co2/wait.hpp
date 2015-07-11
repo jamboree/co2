59dffcb9b5d7c4b1c55b9c7e279e2d6893dcbd80
Jamboree <tongari95@gmail.com>
2015-07-12 00:27:56 +0800

    Improve wait

--- include/co2/wait.hpp
+++ include/co2/wait.hpp
@@ -17,6 +17,10 @@
 
 namespace co2 { namespace wait_detail
 {
+    // The promise_type will be allocated on stack, to ensure that it won't
+    // be destroyed on the caller stack before the awaiter releases it, we
+    // have to lock & notify_one in its destructor instead of in the coroutine
+    // body, so the synchronization stuff are put inside the promise_type.
     struct result
     {
         struct promise_type
@@ -35,23 +39,32 @@ namespace co2 { namespace wait_detail
 
             result get_return_object()
             {
-                return {};
+                return {this};
             }
 
             void set_result() {}
+
+            ~promise_type()
+            {
+                {
+                    std::unique_lock<std::mutex> lock(mtx);
+                    not_ready = false;
+                }
+                cond.notify_one();
+            }
+
+            std::mutex mtx;
+            std::condition_variable cond;
+            bool not_ready = true;
         };
+        promise_type* promise;
     };
 
     template<class Alloc, class Awaitable>
-    auto run(Alloc alloc, Awaitable& a, std::mutex& mtx, std::condition_variable& cond, bool& not_ready)
-    CO2_RET(result, (a, mtx, cond, not_ready), CO2_RESERVE(sizeof(void*));)
+    auto run(Alloc alloc, Awaitable& a)
+    CO2_RET(result, (a), CO2_RESERVE(sizeof(void*));)
     {
         CO2_AWAIT(awaken(a));
-        {
-            std::unique_lock<std::mutex> lock(mtx);
-            not_ready = false;
-        }
-        cond.notify_one();
     } CO2_END_ALLOC(alloc)
 }}
 
@@ -62,13 +75,12 @@ namespace co2
     {
         if (await_ready(a))
             return;
-
-        std::mutex mtx;
-        std::condition_variable cond;
-        bool not_ready = true;
-        std::aligned_storage_t<sizeof(void*) * 9, alignof(std::max_align_t)> buf;
-        wait_detail::run(make_fixed_allocator(buf), a, mtx, cond, not_ready);
-        std::unique_lock<std::mutex> lock(mtx);
+        constexpr auto space = sizeof(wait_detail::result::promise_type) + 6 * sizeof(void*);
+        std::aligned_storage_t<space, alignof(std::max_align_t)> buf;
+        auto promise = wait_detail::run(make_fixed_allocator(buf), a).promise;
+        std::unique_lock<std::mutex> lock(promise->mtx);
+        auto& cond = promise->cond;
+        auto const& not_ready = promise->not_ready;
         while (not_ready) cond.wait(lock);
     }
 
